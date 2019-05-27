d71ca7bee15687eb150dc0ed1f7ab1f7644b7a78
Kawsar Hussen <kawsarhussen@gmail.com>
2019-05-27 03:51:46 -0400

    Plants seed added

--- data/seeds/002_plants.js
+++ data/seeds/002_plants.js
@@ -1,3 +1,73 @@
+
+const faker = require('faker');
+const plants = [
+      'Amaryllis',
+      'Aster',
+      'Anemone',
+      'Azalea',
+      'Begonia',
+      'Bluebell',
+      'Babys Breath',
+      'Chrysanthemum',
+      'Clover',
+      'Crocus',
+      'Freesia',
+      'Gladiola',
+      'Lily',
+      'Daisy',
+      'Bee Balm',
+      'Bergamot',
+      'Bell Flower',
+      'Bird of Paradise',
+      'Bottlebrush',
+      'Calla Lily',
+      'Columbine',
+      'Orchid',
+      'Daffodil',
+      'Primrose',
+      'Forget-Me-Not',
+      'Foxglove',
+      'Iris',
+      'Lilac',
+      'Marjoram',
+      'Orange Blossom',
+      'Peach Blossom',
+      'Petunia',
+      'Rosemary',
+      'Sage',
+      'Thyme',
+      'Thistle',
+      'Hyacinth',
+      "Lady's Slipper",
+      'Amaranthus',
+      'Marigold',
+      'Mimosa',
+      'Peony',
+      'Rose',
+      'Holly',
+      'Lavender',
+      'Snapdragon',
+      'Carnation',
+      'Sunflower',
+      'Tansy',
+      'Tulip',
+      'Buttercup',
+      'Zinnia'
+];
+    
+const getRandomPlantName = () => {
+  return plants[Math.floor(Math.random() * plants.length)];
+};
+    
+
+const seeds = [];
+for (let i = 1; i < 50; i++) {
+  seeds.push({
+    user_id: Math.floor(Math.random() * 50 + 1),
+    name: getRandomPlantName(),
+  });
+}
+
 exports.seed = function(knex, Promise) {
       return knex('plants')
       //.truncate()
@@ -5,6 +75,5 @@ exports.seed = function(knex, Promise) {
       return knex('plants').insert([...seeds]);
       })
 };
-const seeds = [{name: "Rose", user_id: 1} ];
 
-
+    
